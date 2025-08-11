5ef33ba7eb4c7531352fda3950cf84c6986d86aa
MatDgl <danglamathieu@gmail.com>
2025-08-11 16:46:11 +0200

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsEnv = process.env.CORS_ALLOWED_ORIGINS;
+  let corsOrigin: string | RegExp | (string | RegExp)[];
+
+  if (corsEnv && corsEnv.startsWith('/') && corsEnv.endsWith('/')) {
+    // RegExp
+    corsOrigin = new RegExp(corsEnv.slice(1, -1));
+  } else if (corsEnv) {
+    corsOrigin = corsEnv.split(',').map((s) => s.trim());
+  } else {
+    corsOrigin = [];
+  }
+
+  app.enableCors({
+    origin: corsOrigin,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
